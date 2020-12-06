77eb3407abe0867f7eb24d81fab63b9f644575a0
Adam Jiang <jzequn@outlook.com>
2020-12-06 21:17:56 +0800

    update rsg command description

--- bin/index.js
+++ bin/index.js
@@ -1,14 +1,21 @@
 #!/usr/bin/env node
 
 const { program } = require("commander");
-const { createReactComponent } = require("../components/classComponent");
+const { createReactComponent } = require("../components/createReactComponent");
 program.version("2.0.0").name("react-sponge").usage("command");
 
 program
   .command("generate <type> <name>")
-  .description("create react component")
+  .description("create react component", {
+    type:
+      "component type, which only accept one of [class, function, c, f], if wrong value provided, class or c will be chosen instead",
+    name: "the path of the files to be generated",
+  })
   .alias("g")
-  .option("-p,--preprocessor [value]", "css preprocessor to use")
+  .option(
+    "-p,--preprocessor [value]",
+    "extension of css preprocessor to use, e.g. css, scss, less. By default, css is the extension being used"
+  )
   // .option("-t,--typescript", "use typescript instead of javascript")
   .action(function (type, name, options) {
     // console.log(
